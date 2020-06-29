405f541a7adbb1b80a2168a6e36cc495237c7b59
Andrew J. Mauer <ajmauer@gmail.com>
2020-06-29 18:52:46 -0400

    improve performance of first odo push for java-openliberty stack

---
+++ devfiles/openLiberty/baseimage/image-build.sh
@@ -0,0 +1,3 @@
+docker build -t ajymau/ubi8-openjdk -t ajymau/ubi8-openjdk:0.3.0 -f ./Dockerfile-ubi8-openJDK .
+docker build -t ajymau/ubi8-maven -t ajymau/ubi8-maven:0.3.1 -f ./Dockerfile-maven .
+docker build -t ajymau/java-openliberty-odo -f ./Dockerfile-stack .
